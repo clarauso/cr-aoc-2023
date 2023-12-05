b618e36b375590aaebec06588fbe9ecc926b84cb
Claudio Rauso <clarauso@gmail.com>
2023-12-05 21:09:02 +0100

    merged day 1 solutions into single file

--- aoc_1.go
+++ aoc_1.go
@@ -23,7 +23,7 @@ func trebuchet() {
 	scanner := bufio.NewScanner(file)
 	// optionally, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
-		total += handleLineExtended(scanner.Text())
+		total += handleLineExtendedAlt(scanner.Text())
 	}
 
 	if err := scanner.Err(); err != nil {
@@ -97,6 +97,96 @@ func handleLineExtended(line string) int {
 	}
 
 	numString := firstDigit + secondDigit
+	value, _ := strconv.Atoi(numString)
+
+	fmt.Printf("Line [%s] is [%d]\n", line, value)
+
+	return value
+
+}
+
+func handleLineExtendedAlt(line string) int {
+
+	stringDigits := [10]string{"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+
+	var firstDigit string
+	var current string
+	firstIdx := -1
+	for idx, e := range line {
+
+		if firstIdx != -1 {
+			break
+		}
+
+		if unicode.IsDigit(e) {
+			firstDigit = string(e)
+			firstIdx = idx
+			break
+		}
+
+		substring := false
+		current = current + string(e)
+		for digitIdx, digit := range stringDigits {
+
+			if digit == current {
+				firstDigit = string(fmt.Sprintf("%d", digitIdx))
+				firstIdx = idx
+				break
+			} else if strings.Contains(digit, current) {
+				substring = true
+				break
+			}
+
+		}
+
+		if !substring {
+			current = string(e)
+		}
+
+	}
+	// reset the string
+	current = ""
+	var secondDigit string
+	secondIdx := -1
+
+	for idx, e := range line[firstIdx+1:] {
+
+		if unicode.IsDigit(e) {
+			secondDigit = string(e)
+			secondIdx = idx
+			current = ""
+			continue
+		}
+
+		substring := false
+		current = current + string(e)
+		for digitIdx, digit := range stringDigits {
+
+			if digit == current {
+				secondDigit = string(fmt.Sprintf("%d", digitIdx))
+				secondIdx = idx
+				current = ""
+				break
+			} else if strings.Contains(digit, current) {
+				substring = true
+				break
+			}
+
+		}
+
+		if !substring && len(current) > 0 {
+			current = current[1:]
+		}
+
+	}
+
+	var numString string
+	if secondIdx == -1 {
+		numString = firstDigit + firstDigit
+	} else {
+		numString = firstDigit + secondDigit
+	}
+
 	value, _ := strconv.Atoi(numString)
 
 	fmt.Printf("Line [%s] is [%d]\n", line, value)
