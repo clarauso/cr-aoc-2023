cd56ac2779a1bfc50c95e1ca307ef4081577b0de
Claudio Rauso <clarauso@gmail.com>
2023-12-06 08:40:58 +0100

    day 6, part 2 (to be refactored)

--- day06.go
+++ day06.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -19,6 +20,7 @@ func boatRace() {
 	scanner := bufio.NewScanner(file)
 	linesRead := 0
 	var timeSli, distanceSli []int
+	var singleTime, singleDistance int
 	for scanner.Scan() {
 		currentLine := scanner.Text()
 
@@ -26,16 +28,22 @@ func boatRace() {
 		currentLine = strings.ReplaceAll(currentLine, "Time: ", "")
 		currentLine = strings.ReplaceAll(currentLine, "Distance: ", "")
 
+		fmt.Println(currentLine)
+
 		switch {
 		case linesRead == 0:
 			timeSli = mapToArray(currentLine)
+			singleTime = merge(currentLine)
 		case linesRead == 1:
 			distanceSli = mapToArray(currentLine)
+			singleDistance = merge(currentLine)
 		}
 
 		linesRead++
 
 	}
+
+	fmt.Printf("%d %d\n", singleTime, singleDistance)
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
@@ -43,7 +51,6 @@ func boatRace() {
 
 	sol1 := 1
 	for i, _ := range timeSli {
-		fmt.Printf("%d %d\n", timeSli[i], distanceSli[i])
 
 		record := distanceSli[i]
 		wins := 0
@@ -58,7 +65,15 @@ func boatRace() {
 		sol1 *= wins
 	}
 
-	fmt.Printf("Total is %d\n", sol1)
+	sol2 := 0
+	record := singleDistance
+	for j := 0; j < singleTime; j++ {
+		d := distance(singleTime, j)
+		if d > record {
+			sol2++
+		}
+	}
+	fmt.Printf("Total is %d\n", sol2)
 
 }
 
@@ -76,4 +91,14 @@ func distance(raceDuration int, holdTime int) int {
 	remainingTime := raceDuration - holdTime
 
 	return speed(holdTime) * remainingTime
+}
+
+func merge(line string) int {
+
+	line = strings.ReplaceAll(line, " ", "")
+
+	num, _ := strconv.Atoi(line)
+
+	return num
+
 }
